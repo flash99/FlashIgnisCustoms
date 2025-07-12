684181f6f3258628c90b79fcdea90a8ee73475d8
Flash <herrault.david.herrault@gmail.com>
2025-07-13 00:31:18 +0200

    add effect bob & lea

--- script/c999000004.lua
+++ script/c999000004.lua
@@ -1,10 +1,52 @@
 -- c999000004.lua
--- DOGS 01
+-- Bob The Dog
 
 local s,id=GetID()
 
 function s.initial_effect(c)
 
+    -- tohand
+	local e1 = Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON + CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetCountLimit(1, id)
+	e1:SetCondition(aux.TRUE)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+
+    -- tohand
+	local e2 = Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON + CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_REMOVE)
+	e2:SetCountLimit(1, id)
+	e2:SetCondition(aux.TRUE)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
 end
 
 s.listed_series = {SET_DOGS}
+
+function s.thfilter(c)
+	return c:IsSetCard(SET_DOGS) and not c:IsCode(id)
+end
+
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter, tp, LOCATION_ALL, 0, 1, nil) end
+	Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp, LOCATION_ALL)
+end
+
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+	local g = Duel.SelectMatchingCard(tp, s.thfilter, tp, LOCATION_ALL, 0, 1, 1, nil)
+	if #g > 0 then
+        Duel.SpecialSummon(g, SUMMON_TYPE_SPECIAL, tp, tp, 0, 0, POS_FACEUP)
+	end
+end
--- script/c999000005.lua
+++ script/c999000005.lua
@@ -1,10 +1,31 @@
 -- c999000005.lua
--- DOGS 02
+-- Lea The Dog
 
 local s,id=GetID()
 
 function s.initial_effect(c)
 
+    -- pos
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_POSITION)
+	e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_F)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_NEGATE + EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EVENT_DAMAGE_STEP_END)
+	e1:SetCondition(s.poscon)
+	e1:SetOperation(s.posop)
+	c:RegisterEffect(e1)
 end
 
 s.listed_series = {SET_DOGS}
+
+function s.poscon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler()==Duel.GetAttacker() and e:GetHandler():IsRelateToBattle()
+end
+
+function s.posop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsAttackPos() then
+		Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
+	end
+end
